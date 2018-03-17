81cb6350ae376c87bd929dd03a3dedcef404f3bb
Alexey Karetski <karetski@gmail.com>
2018-03-17 13:44:24 +0300

    Initial commit

---
+++ HackAToon/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  HackAToon
+//
+//  Created by Alexey Karetski on 3/17/18.
+//  Copyright © 2018 Karetski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func getStartedButtonTouchUpInside(_ sender: UIButton) {
+        let mapController = MapViewController()
+        navigationController?.pushViewController(mapController, animated: true)
+    }
+}
+
