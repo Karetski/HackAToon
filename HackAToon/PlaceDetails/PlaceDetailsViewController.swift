4e9a8e14afd462c637ce716c41ecdf1a11429751
Alexey Karetski <karetski@gmail.com>
2018-03-17 14:39:14 +0300

    PlaceDetails + gitignore

--- HackAToon/PlaceDetails/PlaceDetailsViewController.swift
+++ HackAToon/PlaceDetails/PlaceDetailsViewController.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2018 Karetski. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PlaceDetailsViewController: UIViewController {
+
+    @IBOutlet weak var previewImage: UIImageView!
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        previewImage.layer.cornerRadius = 24.0
+
+        titleLabel.text = "Sacramento Zoo"
+        subtitleLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        ratingLabel.text = "3.2"
+    }
+    
+}
