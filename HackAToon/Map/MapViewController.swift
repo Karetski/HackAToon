ddc9ac80a506dd905ba86496d2b6283b97bd8e8b
AndreiRybak <andreyrybak0895@gmail.com>
2018-03-17 23:28:05 +0300

    Added annotations.

--- HackAToon/Map/MapViewController.swift
+++ HackAToon/Map/MapViewController.swift
@@ -10,17 +10,17 @@ import Foundation
 import UIKit
 import MapKit
 import CoreLocation
+import Cosmos
 
 class MapViewController: UIViewController {
-    
+
     @IBOutlet private weak var mapView: MKMapView!
     @IBOutlet weak var currentLocationBackground: UIVisualEffectView!
 
     private let locationManager = CLLocationManager()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         setupAppearance()
         setupMap()
     }
@@ -29,10 +29,10 @@ class MapViewController: UIViewController {
         currentLocationBackground.layer.cornerRadius = 8.0
 
     }
-    
+
     private func setupMap() {
         mapView.showsUserLocation = true
-
+        mapView.delegate = self
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
 
@@ -53,24 +53,55 @@ class MapViewController: UIViewController {
     }
 
     @IBAction func currentLocationTouchUpInside(_ sender: UIButton) {
+        var coordinate = mapView.userLocation.coordinate
+        coordinate.latitude = mapView.userLocation.coordinate.latitude
+        coordinate.longitude = mapView.userLocation.coordinate.longitude
+        mapView.addAnnotation(PlaceAnnotation(locationName: "El'feleva bashnya. Paris.", coordinate: coordinate, rating: 3.8))
         moveToCurrentLocation()
     }
 
     private func screenDistance() -> (width: Double, height: Double) {
         let mapRect = self.mapView.visibleMapRect
-        
+
         let eastMapPointX = MKMapPointMake(MKMapRectGetMinX(mapRect), MKMapRectGetMidY(mapRect))
         let westMapPointX = MKMapPointMake(MKMapRectGetMaxX(mapRect), MKMapRectGetMidY(mapRect))
         let screenDistanceWidth = MKMetersBetweenMapPoints(eastMapPointX, westMapPointX)
-        
+
         let northMapPointY = MKMapPointMake(MKMapRectGetMinY(mapRect), MKMapRectGetMidX(mapRect))
         let southMapPointY = MKMapPointMake(MKMapRectGetMaxY(mapRect), MKMapRectGetMidX(mapRect))
         let screenDistanceHeight = MKMetersBetweenMapPoints(northMapPointY, southMapPointY)
-        
+
         return (screenDistanceWidth, screenDistanceHeight)
     }
 }
 
-extension MapViewController: CLLocationManagerDelegate {
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? PlaceAnnotation else {
+            return nil
+        }
 
+        let identifier = "placeAnnotationView"
+        let view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        view.canShowCallout = true
+        
+        let rightButton = UIButton(frame: CGRect(x: 0, y: 0, width: 12, height: 32))
+        rightButton.imageView?.contentMode = .scaleToFill
+        rightButton.setImage(UIImage(named: "GoToAnnotationDetails"), for: .normal)
+        view.rightCalloutAccessoryView = rightButton
+
+        var starSetting = CosmosSettings()
+        starSetting.starMargin = 0
+        starSetting.fillMode = .precise
+        starSetting.updateOnTouch = false
+        starSetting.emptyBorderColor = .clear
+        starSetting.emptyColor = .lightGray
+        starSetting.filledBorderColor = .clear
+        starSetting.filledColor = UIColor(red: 52 / 255, green: 120 / 255, blue: 246 / 255, alpha: 1)
+        let starView = CosmosView(settings: starSetting)
+        starView.rating = annotation.rating
+        view.detailCalloutAccessoryView = starView
+        
+        return view
+    }
 }
