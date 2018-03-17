35acd7112e689d66433b72c542e8eb6b50baf0be
Alexey Karetski <karetski@gmail.com>
2018-03-17 13:55:48 +0300

    Folder structure

---
+++ HackAToon/PlaceDetails/PlaceDetailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PlaceDetailsViewController.swift
+//  HackAToon
+//
+//  Created by Alexey Karetski on 3/17/18.
+//  Copyright © 2018 Karetski. All rights reserved.
+//
+
+import Foundation
---
+++ HackAToon/Search/Temp.swift
@@ -0,0 +1,9 @@
+//
+//  Temp.swift
+//  HackAToon
+//
+//  Created by Alexey Karetski on 3/17/18.
+//  Copyright © 2018 Karetski. All rights reserved.
+//
+
+import Foundation
