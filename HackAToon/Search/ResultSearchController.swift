976d040e32c04c4e210f6849a22b14e89e1c9d2c
Andrei Rybak <Andrei_Rybak@epam.com>
2018-03-17 16:56:31 +0300

    Search result controller

---
+++ HackAToon/Search/ResultSearchController.swift
@@ -0,0 +1,69 @@
+//
+//  ResultSearchController.swift
+//  HackAToon
+//
+//  Created by Andrei Rybak on 3/17/18.
+//  Copyright © 2018 Karetski. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ResultSearchController: UIViewController {
+
+    @IBOutlet private weak var tableView: UITableView! {
+        didSet {
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        }
+    }
+    var matchingItems: [MKMapItem] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.reloadData()
+    }
+}
+
+extension ResultSearchController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchBarText = searchController.searchBar.text else {
+                return
+        }
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = searchBarText
+
+        let search = MKLocalSearch(request: request)
+        search.start { [weak self] response, _ in
+            guard let response = response, let `self` = self else {
+                return
+            }
+
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension ResultSearchController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //TODO: Notify delegate
+        
+    }
+}
+
+extension ResultSearchController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+            return UITableViewCell()
+        }
+        cell.textLabel?.text = matchingItems[indexPath.row].placemark.name
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+}
