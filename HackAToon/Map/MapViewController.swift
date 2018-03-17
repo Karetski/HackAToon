a7b8114a18024acb4f83bc29069582760f83d718
AndreiRybak <andreyrybak0895@gmail.com>
2018-03-17 19:15:52 +0300

    Map view.

--- HackAToon/Map/MapViewController.swift
+++ HackAToon/Map/MapViewController.swift
@@ -6,49 +6,39 @@
 //  Copyright © 2018 Karetski. All rights reserved.
 //
 
+import Foundation
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
-    private var searchResultController: UISearchController?
-
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet fileprivate weak var mapView: MKMapView!
+    
+    fileprivate let locationManager = CLLocationManager()
+    fileprivate var currentLatitude: Double?
+    fileprivate var cuurentLongitude: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setupNavigationBar()
-        title = "Populars"
-        // Do any additional setup after loading the view.
+        setupLocationManager()
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-//        let placeDetailsController = PlaceDetailsViewController()
-//        navigationController?.pushViewController(placeDetailsController, animated: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
     }
-
-    private func setupNavigationBar() {
-        let locationSearchTable = ResultSearchController(nibName: "ResultSearchController", bundle: nil)
-
-        searchResultController = UISearchController(searchResultsController: locationSearchTable)
-        searchResultController?.searchResultsUpdater = locationSearchTable
-        navigationItem.titleView = searchResultController?.searchBar
-        searchResultController?.hidesNavigationBarDuringPresentation = false
-        definesPresentationContext = true
+    
+    internal func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        currentLatitude = location.coordinate.latitude
+        currentLatitude = location.coordinate.longitude
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
+
